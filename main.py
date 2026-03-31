67ffa0096527171d59d7e2f5ad72b1515635c66b
SIMVIA-lucas-sovre <lucas.sovre@simvia.tech>
2026-03-31 13:46:07 +0200

    init code

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
